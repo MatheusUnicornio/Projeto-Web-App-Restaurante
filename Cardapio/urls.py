aa70562dd7ad590eef6e5f4e076fabcd53c810c4
pedrobender0 <pedrobenderprado@gmail.com>
2026-05-12 23:33:06 -0300

    Registrar as URLs do app

--- Cardapio/urls.py
+++ Cardapio/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import view
 
-
+#Link que vai ser codificado no QR Code de cada mesa.
 urlpatterns = [
     path('<int:restaurante_id>/mesa/<int:mesa>/', views.cardapio, name='cardapio'),
 ]
