d3b3cd66c494ac6f77cd41a439960d956d4cad81
pedrobender0 <pedrobenderprado@gmail.com>
2026-05-12 23:52:04 -0300

    Criação base do HTML e tentativa de rodar servidor

--- Cardapio/urls.py
+++ Cardapio/urls.py
@@ -3,5 +3,5 @@ from . import view
 
 #Link que vai ser codificado no QR Code de cada mesa.
 urlpatterns = [
-    path('<int:restaurante_id>/mesa/<int:mesa>/', views.cardapio, name='cardapio'),
+    path('<int:restaurante_id>/mesa/<int:mesa>/', view.cardapio, name='cardapio'),
 ]
